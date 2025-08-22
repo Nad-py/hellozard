717c4036f98d28327128f7c1c6e084f78c8d7da2
Nad-py <naduses@gmail.com>
2025-08-22 23:24:28 +0200

    Any amount of o's anywhere

--- app.js
+++ app.js
@@ -8,28 +8,21 @@ let appIsOn = true;
 ComfyJS.onChat = ( user, message, flags, self, extra ) => {
     const isAllowedToChange = flags["broadcaster"] || (MODPERMISSION == true && flags["mod"]);
     
-    // had to to do this regex because for some reason messages returned with a weird unicode symbol that wasnt being parsed by .trim()
-    /* specifically a weird space at the end
-        U+0020 : SPACE [SP]
-        U+E0000 : <reserved>
-
-        example: "alo 󠀀"
-        regex here is likely flawed, so if you want to fix it then go ahead <3
-    */
-    const cleanedMsg = message.toLowerCase().replace(/[\p{C}\s.!?,'"-]+/gu, '');
+    const lowerMsg = message.toLowerCase();
+    const triggerRegex = /\balo+?\b/i;
     
-    if(cleanedMsg == "stopsayinghello" && isAllowedToChange){
+    if(lowerMsg == "stopsayinghello" && isAllowedToChange){
         appIsOn = false;
     }
-    if(cleanedMsg == "startsayinghello" && isAllowedToChange){
+    if(lowerMsg == "startsayinghello" && isAllowedToChange){
         appIsOn = true;
     }
 
     console.log(JSON.stringify(message));
-    console.log(cleanedMsg);
+    console.log(lowerMsg);
     
 
-    if((cleanedMsg === "alo" || cleanedMsg === "aloo") && appIsOn){
+    if(triggerRegex.test(lowerMsg) && appIsOn){
         let normalizedVolume = !isNaN(VOLUME) ? Math.max(0.01, Math.min(1, VOLUME / 100)) : 1;
         HELLOSOUND.volume = normalizedVolume; 
         HELLOSOUND.currentTime = 0;
